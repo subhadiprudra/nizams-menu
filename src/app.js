7c817af5abd29377fd7ae6c35716863612181e50
subhadiprudra <neloy.nr2@gmail.com>
2022-12-04 13:22:19 +0530

    json corrected

---
+++ src/app.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import background from "./images/backg.jpg";
+import icon from "./images/icon.jpg";
+import {   BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+import jsonData from './items.json';
+const data = JSON.parse(JSON.stringify(jsonData));
+
+
+const arr = ["DAAWAT-E-KHAAS SHURUAAT","MUTTON - KI - MEHEK","LAJAWAB - JALPARI","SUBJIYO - KI - MILAN","MURGH - KI - CHAHAT","DUM - E - BASMATI","ROTI - KA - KHAZANA","NIZAM' S SPECIAL KABAB ROLL'S","ACCOMPANIMENTS","NIZAM'S - KI - MITHAS","RAMJAN SPECIAL"];
+  
+  class ItemTypes extends React.Component{
+    render(){
+        const t = this.props.type;
+        const rows = [];
+        for (let i = 0; i < arr.length; i++) {
+            rows.push(<ItemType v={arr[i]} index={i} type= {t}/>);
+        }
+        return  <div><div>{rows}</div></div>
+    }
+  }
+  
+  class ItemType extends React.Component{
+
+   render(){
+
+        const x = this.props.v;
+        const i = this.props.index;
+        const items = data[i][String(x)];
+        const type = this.props.type;
+
+        console.log(items);
+
+        const rows = [];
+        for (let i = 0; i < items.length; i++) {
+            if(type=="ac"){
+                rows.push(<div className='item'><div className='item_name'>{items[i]['name']}</div><div className='cost'> {"₹"+items[i]['ac_cost']}</div></div>);
+            }else{
+                rows.push(<div className='item'><div className='item_name'>{items[i]['name']}</div><div className='cost'> {"₹"+items[i]['nonac_cost']}</div></div>);
+            }
+        }
+        
+        return <div className='item_type'>
+            <div className='cat_name'>{x}</div>
+            <div className='item_box'>{rows}</div>
+           
+          </div>
+   }
+
+
+  }
+
+  class ACMENU extends React.Component{
+    render(){
+     return (
+        <div>
+          <div style={{display : 'flex'}}>
+            <img className= 'icon' src={icon}/>
+            <div  className='title'>MENU CARD (AC)</div>
+          </div>
+          <ItemTypes type='ac'/>
+        </div>
+      );
+    }
+  }
+
+  class NONACMENU extends React.Component{
+    render(){
+     return (
+        <div>
+          <div style={{display : 'flex'}}>
+            <img className= 'icon' src={icon}/>
+            <div  className='title'>MENU CARD (NON AC)</div>
+          </div>
+          <ItemTypes types='nonac'/>
+          
+        </div>
+      );
+    }
+  }
+  
+  export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/ac' element={<ACMENU/>} />
+                <Route path='/nonac' element={<NONACMENU/>} />
+            </Routes>
+            
+        </Router>
+    );
+  }
--- src/index.js
+++ src/index.js
@@ -2,96 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import background from "./images/backg.jpg";
-import icon from "./images/icon.jpg";
-import StackView from 'react-stack-view';
- 
-
-import jsonData from './items.json';
-const data = JSON.parse(JSON.stringify(jsonData));
-class Square extends React.Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-          v: props.v,
-        };
-    }
-
-    render() {
-      return (
-        <button className="square" onClick={()=>this.props.onClick()}>
-          {this.state.v}
-        </button>
-      );
-    }
-  }
-
-  const arr = ["DAAWAT-E-KHAAS SHURUAAT","Kathi Kabab Roll'S","Tandoori Kebab'S","Veg Gravy"]
-  
-  class ItemTypes extends React.Component{
-    render(){
-        const rows = [];
-        for (let i = 0; i < arr.length; i++) {
-            rows.push(<ItemType v={arr[i]} index={i}/>);
-        }
-        return  <div><div>{rows}</div></div>
-    }
-  }
-  
-  class ItemType extends React.Component{
-
-   render(){
-
-        const x = this.props.v;
-        const i = this.props.index;
-        const items = data[i][String(x)];
-
-        console.log(items);
-
-        const rows = [];
-        for (let i = 0; i < items.length; i++) {
-            rows.push(<div className='item'><div className='item_name'>{items[i]['name']}</div><div className='cost'> {"₹"+items[i]['cost']}</div></div>);
-        }
-        
-        return <div className='item_type'>
-            <div className='cat_name'>{x}</div>
-            <div className='item_box'>{rows}</div>
-           
-          </div>
-   }
+import App from './app';
 
 
-  }
-  
-
-  class ItemTyp extends React.Component{
-    render(){
-      this.ref.stack.push(<ItemTypes/>);
-
-
-      return <StackView ref="stack">
-      
-      </StackView>
-    }
-  }
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<div >
-    <div style={{ 
-    backgroundImage: `url(${background})`,
-        height:'100vh',
-        width:'200vh',
-        backgroundSize: 'cover',
-        position:'fixed',
-        backgroundRepeat: 'no-repeat',
-        padding: '5%',
-        zIndex:'-99'
-  }}></div>
-    <div style={{display : 'flex'}}>
+  root.render(
    
-    <img className= 'icon' src={icon}/>
-    <div  className='title'>MENU CARD</div>
+    <div >
+      <div style={{ 
+      backgroundImage: `url(${background})`,
+          height:'100vh',
+          width:'200vh',
+          backgroundSize: 'cover',
+          position:'fixed',
+          backgroundRepeat: 'no-repeat',
+          padding: '5%',
+          zIndex:'-99'
+    }}>
+      </div>
+      
+      <App/>
+      
     </div>
-    <ItemTypes/>
-    </div>);
+    
+  );
   
